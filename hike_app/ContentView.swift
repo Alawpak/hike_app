5798355084f88abbdd059a7f6d777a8fac04ecb6
Alawpak <alan281@hotmail.es>
2024-11-12 14:05:02 -0600

    Initial Commit

---
+++ hike_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hike_app
+//
+//  Created by Alan Perez on 12/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hike_app/hike_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  hike_appApp.swift
+//  hike_app
+//
+//  Created by Alan Perez on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct hike_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
