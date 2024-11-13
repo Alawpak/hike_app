36d0f639428eb17af2dff4b41d021cf0335e4feb
Alawpak <alan281@hotmail.es>
2024-11-12 19:38:21 -0600

    feat: add header

--- hike_app/Component/CardView.swift
+++ hike_app/Component/CardView.swift
@@ -10,21 +10,56 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         // MARK: - CARD
-        
+
         ZStack {
             CustomBackgroundView()
-            
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(
-                            colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")],
-                            startPoint: .topLeading, endPoint: .bottomLeading)
-                    )
-                    .frame(width: 256, height: 256)
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+
+            VStack {
+                // MARK: - HEADER
+
+                VStack(alignment: .leading) {
+
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [.customGrayLight, .customGrayMedium],
+                                startPoint: .top, endPoint: .bottom))
+                        
+                        Spacer()
+                        
+                        Button{
+                            print("hola")
+                        }label: {
+                            CustomButtonView()
+                        }
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(
+                            .leading
+                        )
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }  //: HEADER
+                .padding(.horizontal, 30)
+                // MARK: - MAIN CONTENT
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")],
+                                startPoint: .topLeading, endPoint: .bottomLeading)
+                        )
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+
+                //MARK: - FOOTER
             }
         }
         .frame(width: 320, height: 570)
