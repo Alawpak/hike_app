7dada30966f2a903fc92711ebeef1a05adf0ef0d
Alawpak <alan281@hotmail.es>
2024-11-13 13:07:17 -0600

    feat: change app's icon

--- hike_app/Screen/SettingsView.swift
+++ hike_app/Screen/SettingsView.swift
@@ -79,7 +79,18 @@ struct SettingsView: View {
                     HStack(spacing: 12) {
                         ForEach(alternateAppIcons.indices, id: \.self) { item in
                             Button {
-                                print("Icon was pressed.")
+                                UIApplication.shared.setAlternateIconName(alternateAppIcons[item]) {
+                                    error in
+                                    if error != nil {
+                                        print(
+                                            "Failed request to update the app's icon: \(String(describing: error?.localizedDescription))"
+                                        )
+                                    } else {
+                                        print(
+                                            "Success! You have changed the app's icon to \(alternateAppIcons[item])"
+                                        )
+                                    }
+                                }
                             } label: {
                                 Image("\(alternateAppIcons[item])-Preview")
                                     .resizable()
